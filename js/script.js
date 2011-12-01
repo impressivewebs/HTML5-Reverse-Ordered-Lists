725005eead49aa97c8968e80041d81928c2f96b8
Louis Lazaris <lazaris@impressivewebs.com>
2011-12-01 02:23:04 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function(){
+	// run the code on each ordered list with a 'reversed' attribute.
+	var myLists = $('ol[reversed]'),
+		currCount = null,
+		currChildren = null;
+
+	$(myLists).each(function() {
+		
+		// check the existence of the start attribute
+		if ($(this).attr('start')) {
+			
+			// if it exists, convert it to an integer
+			currCount = parseInt($(this).attr('start'), 10);
+			
+			// If it wasn't a number, it will return 'NaN'; if so, use the number of list items instead
+			if (isNaN(currCount)) {
+				currCount = $(this).children().size();
+			} else {
+				// It's a number, so round it, just in case some sicko tries to use a decimal value
+				currCount = Math.round(currCount);
+			}
+		
+		// do this if the start attribute is not present
+		// the first number is derived from the number of list items
+		} else {
+			currCount = $(this).children().size();
+		}
+		
+		// grab all the child list items
+		currChildren = $(this).children();
+		
+		// go through each list item, adding the 'value' attribute plus currCount number
+		// then subract one from currCount so we're ready for the next one
+		$(currChildren).each(function() {
+
+		   $(this).attr('value', currCount);
+		   currCount = currCount - 1;
+
+		});
+		
+	});
+});
